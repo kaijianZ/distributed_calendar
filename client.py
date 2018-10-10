f1452cca55f853db3c0188381a50a3eadce98dfa
kaijianZ <zhongk@rpi.edu>
2018-10-10 02:33:47 -0400

    Finished a UDP echo demo with asyncio; Some helper function and class;

---
+++ client.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import asyncio
+from aioconsole import ainput
+
+
+class CalenderServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        print('Received %r from %s' % (message, addr))
+        print('Send %r to %s' % (message, addr))
+        self.transport.sendto(data, addr)
+
+
+async def hello():
+    while True:
+        line = await ainput('>>> ')
+        print(line)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    print("Starting UDP server")
+    # One protocol instance will be created to serve all client requests
+    listen = loop.create_datagram_endpoint(
+        CalenderServerProtocol, local_addr=('127.0.0.1', 9999))
+    tasks = [hello(), listen]
+    loop.run_until_complete(asyncio.wait(tasks))
---
+++ log.py
@@ -0,0 +1,15 @@
+class Meeting(object):
+    def __init__(self, name, day, start, end, participants):
+        self.name = name
+        self.day = day
+        self.start = start
+        self.end = end
+        self.participants = participants
+
+    def participating(self, user):
+        return user in self.participants
+
+    def conflict(self, other_meeting):
+        pass
+
+class Log(object):
