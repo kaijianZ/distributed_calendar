8f1caeeb9284aed39596cb017124ed1d52343923
kaijianZ <zhongk@rpi.edu>
2018-10-14 03:57:17 -0400

    recv finished

--- client.py
+++ client.py
@@ -24,7 +24,7 @@ class CalenderServerProtocol:
         i = host_num[node]
         t_k, NP = pickle.loads(data)
         NE = list(filter(lambda rec: not has_rec(matrix_clock, rec,
-                                                    i), NP))
+                                                 i), NP))
         insert_set = set()
         delete_set = set()
         for l in NE:
@@ -36,12 +36,12 @@ class CalenderServerProtocol:
         for insert_meeting in insert_set:
             if insert_meeting.name not in delete_set:
                 for meeting in sorted_view(filter_by_participants(
-                            calender.values(), node)):
+                        calender.values(), node)):
                     if insert_meeting.conflict(meeting) and \
-                                insert_meeting.name > meeting.name:
+                            insert_meeting.name > meeting.name:
                         waiting_delete.add(insert_meeting.name)
                         continue
-                    elif insert_meeting.conflict(meeting) and meeting.name\
+                    elif insert_meeting.conflict(meeting) and meeting.name \
                             not in delete_set:
                         waiting_delete.add(meeting.name)
                 calender[insert_meeting.name] = insert_meeting
@@ -71,85 +71,82 @@ class CalenderServerProtocol:
             for host in participants:
                 if host != node:
                     send_log(matrix_clock, logs, host, hosts[host],
-                                 host_num)
+                             host_num)
             print(f'Meeting {name} cancelled.')
-
-
 
 
 async def hello():
     while True:
         line = await ainput()
 
-        async with lock:
-            global calender
-            global node
-            global counter
-            global matrix_clock
-            global logs
-            global hosts
-            global host_num
-            if line == 'view':
-                for meeting in sorted_view(calender.values()):
-                    print(meeting)
+        global calender
+        global node
+        global counter
+        global matrix_clock
+        global logs
+        global hosts
+        global host_num
+        if line == 'view':
+            for meeting in sorted_view(calender.values()):
+                print(meeting)
 
-            elif line == 'myview':
-                for meeting in sorted_view(filter_by_participants(
-                        calender.values(), node)):
-                    print(meeting)
+        elif line == 'myview':
+            for meeting in sorted_view(filter_by_participants(
+                    calender.values(), node)):
+                print(meeting)
 
-            elif line == 'log':
-                for l in logs:
-                    print(l)
+        elif line == 'log':
+            for l in logs:
+                print(l)
 
-            else:
-                line = line.split(' ')
+        else:
+            line = line.split(' ')
 
-                if line[0] == 'schedule':
-                    line = line[1:]
-                    name = line[0]
-                    day = datetime.strptime(line[1], "%m/%d/%Y").date()
-                    start = datetime.strptime(line[2], "%H:%M").time()
-                    end = datetime.strptime(line[3], "%H:%M").time()
-                    participants = line[4].split(',')
+            if line[0] == 'schedule':
+                line = line[1:]
+                name = line[0]
+                day = datetime.strptime(line[1], "%m/%d/%Y").date()
+                start = datetime.strptime(line[2], "%H:%M").time()
+                end = datetime.strptime(line[3], "%H:%M").time()
+                participants = line[4].split(',')
 
-                    new_meeting = Meeting(name, day, start, end, participants)
+                new_meeting = Meeting(name, day, start, end, participants)
 
-                    if ok_to_schedule(calender.values(), new_meeting):
-                        calender[name] = new_meeting
-                        counter += 1
-                        matrix_clock[host_num[node]][host_num[node]] = counter
-                        new_log = Log('create', counter, host_num[node],
-                                      new_meeting)
-                        logs.append(new_log)
-                        print('Meeting', name, 'scheduled.')
-                        for host in participants:
-                            if host != node:
-                                send_log(matrix_clock, logs, host, hosts[host],
-                                         host_num)
+                if ok_to_schedule(calender.values(), new_meeting):
+                    calender[name] = new_meeting
+                    counter += 1
+                    matrix_clock[host_num[node]][host_num[node]] = counter
+                    new_log = Log('create', counter, host_num[node],
+                                  new_meeting)
+                    logs.append(new_log)
+                    print('Meeting', name, 'scheduled.')
+                    for host in participants:
+                        if host != node:
+                            send_log(matrix_clock, logs, host, hosts[host],
+                                     host_num)
 
-                    else:
-                        print('Unable to schedule meeting', name + '.')
+                else:
+                    print('Unable to schedule meeting', name + '.')
 
-                elif line[0] == 'cancel':
-                    name = line[1]
-                    # when the user is the only one in the event
-                    if name not in calender:
-                        print("nah")
+            elif line[0] == 'cancel':
+                name = line[1]
+                # when the user is the only one in the event
+                if name not in calender:
+                    print("nah")
 
-                    else:
-                        # deletes the event in the schedule
-                        participants = calender[name].participants
-                        del calender[name]
-                        counter += 1
-                        matrix_clock[host_num[node]][host_num[node]] = counter
-                        new_log = Log('delete', counter, host_num[node], name)
-                        logs.append(new_log)
-                        for host in participants:
-                            if host != node:
-                                send_log(matrix_clock, logs, host, hosts[host],
-                                         host_num)
-                        print(f'Meeting {name} cancelled.')
+                else:
+                    # deletes the event in the schedule
+                    participants = calender[name].participants
+                    del calender[name]
+                    counter += 1
+                    matrix_clock[host_num[node]][host_num[node]] = counter
+                    new_log = Log('delete', counter, host_num[node], name)
+                    logs.append(new_log)
+                    for host in participants:
+                        if host != node:
+                            send_log(matrix_clock, logs, host, hosts[host],
+                                     host_num)
+                    print(f'Meeting {name} cancelled.')
 
 
 if __name__ == "__main__":
@@ -175,7 +172,6 @@ if __name__ == "__main__":
 
     matrix_clock = [[0 for _ in range(len(hosts))] for _ in range(len(hosts))]
 
-    lock = asyncio.Lock()
     loop = asyncio.get_event_loop()
     print("Starting UDP server")
     # One protocol instance will be created to serve all client requests
